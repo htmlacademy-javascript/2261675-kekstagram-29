42c0267492241060b777ae45e58246cbb3acbc66
Vladislav <ma1rosssss@gmail.com>
2023-06-20 14:13:00 +0300

    добавляет массив

---
+++ js/main.js
@@ -0,0 +1,44 @@
+const ID_PHOTO = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25];
+
+const urlPhoto = [
+  'photos/1.jpg',
+  'photos/2.jpg',
+  'photos/3.jpg',
+  'photos/4.jpg',
+  'photos/5.jpg',
+  'photos/6.jpg',
+  'photos/7.jpg',
+  'photos/8.jpg',
+  'photos/9.jpg',
+  'photos/10.jpg',
+  'photos/11.jpg',
+  'photos/12.jpg',
+  'photos/13.jpg',
+  'photos/14.jpg',
+  'photos/15.jpg',
+  'photos/16.jpg',
+  'photos/17.jpg',
+  'photos/18.jpg',
+  'photos/19.jpg',
+  'photos/20.jpg',
+  'photos/21.jpg',
+  'photos/22.jpg',
+  'photos/23.jpg',
+  'photos/24.jpg',
+  'photos/25.jpg'
+];
+
+const description_photo  = [
+  'очень красиво',
+  'смазанная фотка',
+  ''
+];
+
+const avatar = [
+  img/avatar-1.svg,
+  img/avatar-2.svg,
+  img/avatar-3.svg,
+  img/avatar-4.svg,
+  img/avatar-5.svg,
+  img/avatar-6.svg,
+];
