f902c1c594b290fe74b5e559a0e8128b45437129
Vladislav <ma1rosssss@gmail.com>
2023-06-21 15:49:02 +0300

    создает программу

--- js/main.js
+++ js/main.js
@@ -1,44 +1,82 @@
-const ID_PHOTO = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25];
 
-const urlPhoto = [
-  'photos/1.jpg',
-  'photos/2.jpg',
-  'photos/3.jpg',
-  'photos/4.jpg',
-  'photos/5.jpg',
-  'photos/6.jpg',
-  'photos/7.jpg',
-  'photos/8.jpg',
-  'photos/9.jpg',
-  'photos/10.jpg',
-  'photos/11.jpg',
-  'photos/12.jpg',
-  'photos/13.jpg',
-  'photos/14.jpg',
-  'photos/15.jpg',
-  'photos/16.jpg',
-  'photos/17.jpg',
-  'photos/18.jpg',
-  'photos/19.jpg',
-  'photos/20.jpg',
-  'photos/21.jpg',
-  'photos/22.jpg',
-  'photos/23.jpg',
-  'photos/24.jpg',
-  'photos/25.jpg'
+
+const ID_PHOTO = 25;
+const AVATAR = 6;
+const LIKES_MIN = 15;
+const LIKES_MAX = 200;
+const COMMENTS_MAX = 30;
+const COMMENT_PHOTO = [
+  'Очень красиво',
+  'Смазанная фотка',
+  'Прекрасный снимок',
+  'Шикарный вид'
 ];
 
-const description_photo  = [
-  'очень красиво',
-  'смазанная фотка',
-  ''
+const MESSAGE = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
 
-const avatar = [
-  img/avatar-1.svg,
-  img/avatar-2.svg,
-  img/avatar-3.svg,
-  img/avatar-4.svg,
-  img/avatar-5.svg,
-  img/avatar-6.svg,
+const AUTOR = [
+  'Артем',
+  'Витя',
+  'Дима',
+  'Даша',
+  'Ирина'
 ];
+
+
+const getRandomInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+const getRandomArrayElement = (items) =>
+  items[getRandomInteger(0, items.length - 1)];
+
+const createIdGenerator = () => {
+  let lastGeneratedID = 0;
+  return () => {
+    lastGeneratedID += 1;
+    return lastGeneratedID;
+  };
+};
+
+const generateCommentId = createIdGenerator();
+
+const createMessage = () => getRandomInteger(0, 2)
+  ? getRandomArrayElement(COMMENT_PHOTO)
+  : `${getRandomArrayElement(COMMENT_PHOTO)} ${getRandomArrayElement(COMMENT_PHOTO)}`;
+
+
+const createComment = () => ({
+  id: generateCommentId(),
+  avatar: `img/avatar-${getRandomInteger(1, AVATAR)}.svg`,
+  message: createMessage(),
+  name: getRandomArrayElement(AUTOR),
+});;
+
+const createPicture = (index) => ({
+  id: index,
+  url: `photos/${index}.jpg`,
+  description: getRandomArrayElement(COMMENT_PHOTO),
+  likes: getRandomInteger(LIKES_MIN, LIKES_MAX),
+  comment: Array.from(
+    {length: getRandomInteger(0, COMMENTS_MAX) },
+    createComment,
+  ),
+});
+
+
+const getPictures = () => Array.from(
+  { length: ID_PHOTO },
+  (_, pictureIndex) => createPicture(pictureIndex + 1),
+);
+
+getPictures();
