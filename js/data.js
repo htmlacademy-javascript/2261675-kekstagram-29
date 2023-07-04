ca840d21ddbf67fa30fd12b7c408c401c191f1b2
Vladislav <ma1rosssss@gmail.com>
2023-07-04 16:31:15 +0300

    делает правки

--- js/data.js
+++ js/data.js
@@ -29,25 +29,6 @@ const AUTOR = [
   'Ирина'
 ];
 
-
-// const getRandomInteger = (a, b) => {
-//   const lower = Math.ceil(Math.min(a, b));
-//   const upper = Math.floor(Math.max(a, b));
-//   const result = Math.random() * (upper - lower + 1) + lower;
-//   return Math.floor(result);
-// };
-
-// const getRandomArrayElement = (items) =>
-//   items[getRandomInteger(0, items.length - 1)];
-
-// const createIdGenerator = () => {
-//   let lastGeneratedID = 0;
-//   return () => {
-//     lastGeneratedID += 1;
-//     return lastGeneratedID;
-//   };
-// };
-
 const generateCommentId = createIdGenerator();
 
 const createMessage = () => Array.from(
